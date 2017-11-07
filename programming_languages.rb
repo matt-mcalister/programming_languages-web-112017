b1f4d97a0792cff6485521b8d5d9da2f5740ebd2
Matt McAlister <matt.mcalister93@gmail.com>
2017-11-07 19:40:41 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,55 @@
-def reformat_languages(languages)
-  # your code here
+require "pry"
+
+def languages
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+}
 end
+
+def reformat_languages(languages)
+  new_hash = {}
+
+  languages.each do |style, data|
+    binding.pry
+
+    data.each do |language, type_info|
+      binding.pry
+
+      type_info.each do |type, value|
+        binding.pry
+
+      end
+    end
+  end
+  
+end
+
+reformat_languages
