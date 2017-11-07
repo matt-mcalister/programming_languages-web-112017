9ba3bf3f9b5f437bf25c821025de66f5c033d9c6
Matt McAlister <matt.mcalister93@gmail.com>
2017-11-07 19:46:41 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,55 +1,62 @@
 require "pry"
 
-def languages
-languages = {
-  :oo => {
-    :ruby => {
-      :type => "interpreted"
-    },
-    :javascript => {
-      :type => "interpreted"
-    },
-    :python => {
-      :type => "interpreted"
-    },
-    :java => {
-      :type => "compiled"
-    }
-  },
-  :functional => {
-    :clojure => {
-      :type => "compiled"
-    },
-    :erlang => {
-      :type => "compiled"
-    },
-    :scala => {
-      :type => "compiled"
-    },
-    :javascript => {
-      :type => "interpreted"
-    }
-
-  }
-}
-end
+# def languages
+# languages = {
+#   :oo => {
+#     :ruby => {
+#       :type => "interpreted"
+#     },
+#     :javascript => {
+#       :type => "interpreted"
+#     },
+#     :python => {
+#       :type => "interpreted"
+#     },
+#     :java => {
+#       :type => "compiled"
+#     }
+#   },
+#   :functional => {
+#     :clojure => {
+#       :type => "compiled"
+#     },
+#     :erlang => {
+#       :type => "compiled"
+#     },
+#     :scala => {
+#       :type => "compiled"
+#     },
+#     :javascript => {
+#       :type => "interpreted"
+#     }
+# 
+#   }
+# }
+# end
 
 def reformat_languages(languages)
   new_hash = {}
 
   languages.each do |style, data|
-    binding.pry
+    # binding.pry
 
     data.each do |language, type_info|
-      binding.pry
+      # binding.pry
 
       type_info.each do |type, value|
-        binding.pry
-
+        # binding.pry
+        if new_hash[language] == nil
+          new_hash[language] = {
+            type: value,
+            style: [style]
+          }
+        else
+          new_hash[language][:style] << style
+        end
       end
     end
   end
 
 end
 
-reformat_languages(languages)
+# reformat_languages(languages)
